3cfee3fbdc7a5a6f0e564d9c485e5e230f24a09a
Abdullah Azayem Ali Mohamed <70292694+AbdullahAzayem@users.noreply.github.com>
2021-05-24 23:47:18 +0200

    Add files via upload

---
+++ app_main.c
@@ -0,0 +1,30 @@
+#include "app_main.h"
+
+
+char u8InitGLobalVar = 2;
+static long int u32NonInitStaticGlobalVar; 
+const short int u16ConstGlobalVar = 3;
+
+#define DIR __attribute__((__section__(".sprints"))) 
+
+int DIR arr[10]={0};
+
+void main(void)
+{
+
+	short int u16LocVar = 4;
+	static char u8InitStaticLocVar = 5;
+	if(u16ConstGlobalVar == 3)
+	{
+		u32NonInitStaticGlobalVar = 6;
+		vidFunc(&u16LocVar);
+	}
+	else
+	{
+		u32NonInitStaticGlobalVar = 7;
+	}
+	while(1)
+	{
+	
+	}
+}
